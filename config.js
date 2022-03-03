9b1cfd438992a6ca711eafd512476a48e91dd77e
Arush-kshirsagar <71516847+Arush-kshirsagar@users.noreply.github.com>
2022-03-03 10:40:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlmBApE6Zzw-uHLst0pFEK9YsQyb-_rrM",
+  authDomain: "e-bike-rider-4.firebaseapp.com",
+  projectId: "e-bike-rider-4",
+  storageBucket: "e-bike-rider-4.appspot.com",
+  messagingSenderId: "259644441739",
+  appId: "1:259644441739:web:74602ed1aae1d01933c08e"
+};
+  
+
+
+const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
